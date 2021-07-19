77e9845c6053d78d1c147c979fa7ce909f1e6953
sanbo <xueyongfu@analysys.com.cn>
2021-07-19 14:29:55 +0800

    android T crash

--- app/src/main/java/com/github/qiang/iproute2/parsemac/MainActivity.java
+++ app/src/main/java/com/github/qiang/iproute2/parsemac/MainActivity.java
@@ -1,28 +1,35 @@
 package com.github.qiang.iproute2.parsemac;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     static {
         System.loadLibrary("native-lib");
     }
 
+    public native String parseMac();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Button btn = new Button(this);
+        btn.setText("mac");
+        setContentView(btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = parseMac();
+                Log.i("Q_M", "MAC:" + s);
+            }
+        });
     }
 
-    public native String parseMac();
 
-    public void click(View view) {
-        String s = parseMac();
-        Log.i("Q_M", "MAC:" + s);
-    }
 }
